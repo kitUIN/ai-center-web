90ef756781ba5422ca0a5d811e8864cc6f2322ce
kitUIN <kulujun@gmail.com>
2024-11-04 17:25:41 +0800

    feat | Base Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import * as React from "react";
 import {
-  AppItem,
   Hamburger,
-  NavCategory,
-  NavCategoryItem,
-  NavDivider,
   NavDrawer,
   NavDrawerBody,
   NavDrawerHeader,
@@ -12,14 +8,9 @@ import {
   NavItem,
   NavItemValue,
   NavSectionHeader,
-  NavSubItem,
-  NavSubItemGroup,
   OnNavItemSelectData,
 } from "@fluentui/react-nav-preview";
-import {
-  Button,
-  Label,
-  Switch,
+import { 
   Tooltip,
   makeStyles,
   tokens,
@@ -30,32 +21,17 @@ import {
   Board20Regular,
   BoxMultiple20Filled,
   BoxMultiple20Regular,
-  DataArea20Filled,
-  DataArea20Regular,
-  DocumentBulletListMultiple20Filled,
-  DocumentBulletListMultiple20Regular,
-  HeartPulse20Filled,
-  HeartPulse20Regular,
   MegaphoneLoud20Filled,
   MegaphoneLoud20Regular,
-  NotePin20Filled,
-  NotePin20Regular,
   People20Filled,
   People20Regular,
-  PeopleStar20Filled,
-  PeopleStar20Regular,
-  Person20Filled,
   PersonLightbulb20Filled,
   PersonLightbulb20Regular,
-  Person20Regular,
-  PersonSearch20Filled,
-  PersonSearch20Regular,
-  PreviewLink20Filled,
-  PreviewLink20Regular,
   bundleIcon,
-  PersonCircle32Regular,
 } from "@fluentui/react-icons";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
 const useStyles = makeStyles({
   root: {
     overflow: "hidden",
@@ -78,85 +54,18 @@ const useStyles = makeStyles({
   },
 });
 
-const Person = bundleIcon(Person20Filled, Person20Regular);
 const Dashboard = bundleIcon(Board20Filled, Board20Regular);
 const Announcements = bundleIcon(MegaphoneLoud20Filled, MegaphoneLoud20Regular);
 const EmployeeSpotlight = bundleIcon(
   PersonLightbulb20Filled,
   PersonLightbulb20Regular
 );
-const Search = bundleIcon(PersonSearch20Filled, PersonSearch20Regular);
-const PerformanceReviews = bundleIcon(
-  PreviewLink20Filled,
-  PreviewLink20Regular
-);
-const JobPostings = bundleIcon(NotePin20Filled, NotePin20Regular);
 const Interviews = bundleIcon(People20Filled, People20Regular);
-const HealthPlans = bundleIcon(HeartPulse20Filled, HeartPulse20Regular);
 const TrainingPrograms = bundleIcon(BoxMultiple20Filled, BoxMultiple20Regular);
-const CareerDevelopment = bundleIcon(PeopleStar20Filled, PeopleStar20Regular);
-const Analytics = bundleIcon(DataArea20Filled, DataArea20Regular);
-const Reports = bundleIcon(
-  DocumentBulletListMultiple20Filled,
-  DocumentBulletListMultiple20Regular
-);
 
-// A type that represents a navItemValue and its potential children.
-// An empty children array indicates a Single top level NavItem.
-// A hydrated children array indicates a NavCategoryItem with children.
-type NavItemValueCombo = { parent: string; children: string[] };
-
-// This is a list of navItemValues and their potential children
-// Ite exactly matches the NavDrawer in the story below
-// This is how a consumer might store them in their app
-const navItemValueList: NavItemValueCombo[] = [
-  { parent: "1", children: [] },
-  { parent: "2", children: [] },
-  { parent: "3", children: [] },
-  { parent: "4", children: [] },
-  { parent: "5", children: [] },
-  { parent: "6", children: ["7", "8"] },
-  { parent: "9", children: [] },
-  { parent: "10", children: [] },
-  { parent: "11", children: ["12", "13"] },
-  { parent: "14", children: [] },
-  { parent: "15", children: [] },
-  { parent: "16", children: ["17", "18"] },
-  { parent: "19", children: [] },
-  { parent: "20", children: [] },
-];
-
-type SelectedPage = {
-  newSelectedCategory: string | undefined;
-  newSelectedValue: string;
-};
-
-const getRandomPage = (): SelectedPage => {
-  const randomIndex = Math.floor(Math.random() * navItemValueList.length);
-  const randomItem = navItemValueList[randomIndex];
-
-  // there are no children, so we're selecting a top level item
-  if (randomItem.children.length === 0) {
-    return {
-      newSelectedCategory: undefined,
-      newSelectedValue: randomItem.parent,
-    };
-  } else {
-    // there are children, so we're including a category and it's child as the selectedValue
-    const randomChildIndex = Math.floor(
-      Math.random() * randomItem.children.length
-    );
-    return {
-      newSelectedCategory: randomItem.parent,
-      newSelectedValue: randomItem.children[randomChildIndex],
-    };
-  }
-};
-
+ 
 export const App = (props: Partial<NavDrawerProps>) => {
   const styles = useStyles();
-
-  const multipleLabelId = useId("multiple-label");
 
   const [openCategories, setOpenCategories] = React.useState<NavItemValue[]>([
     "6",
@@ -216,25 +125,7 @@ export const App = (props: Partial<NavDrawerProps>) => {
     );
   };
 
-  const handleNavigationClick = () => {
-    const { newSelectedCategory, newSelectedValue } = getRandomPage();
-
-    setSelectedCategoryValue(newSelectedCategory);
-    setSelectedValue(newSelectedValue);
-  };
-
-  const handleMultipleChange = (
-    ev: Event | React.SyntheticEvent<Element, Event>,
-    data: { checked: boolean }
-  ) => {
-    setIsMultiple(data.checked);
-
-    if (data.checked) {
-      setOpenCategories(["6", "11"]);
-    } else {
-      setOpenCategories(["6"]);
-    }
-  };
+ 
 
   return (
     <div className={styles.root}>
@@ -259,10 +150,10 @@ export const App = (props: Partial<NavDrawerProps>) => {
           {/* <AppItem icon={<PersonCircle32Regular />} as="a">
             Contoso HR
           </AppItem> */}
-          <NavItem icon={<Dashboard />} value="1">
+          <NavItem href="/" icon={<Dashboard />} value="1">
             看板
           </NavItem>
-          <NavItem icon={<Announcements />} value="2">
+          <NavItem href="/about" icon={<Announcements />} value="2">
             数据集
           </NavItem>
           <NavItem icon={<EmployeeSpotlight />} value="3">
@@ -278,20 +169,12 @@ export const App = (props: Partial<NavDrawerProps>) => {
           </NavItem>
         </NavDrawerBody>
       </NavDrawer>
-      <div className={styles.content}>
-        <div className={styles.field}>
-          <Button appearance="primary" onClick={handleNavigationClick}>
-            Navigate
-          </Button>
-          <Label id={multipleLabelId}>Categories</Label>
-          <Switch
-            checked={isMultiple}
-            onChange={(_, data) => handleMultipleChange(_, data)}
-            label={isMultiple ? "Multiple" : "Single"}
-            aria-labelledby={multipleLabelId}
-          />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+		</BrowserRouter> 
     </div>
   );
 };
