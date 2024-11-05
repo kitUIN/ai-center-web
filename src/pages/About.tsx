516cbfcd153b1fd389ffec88deebbbe9663feec2
kitUIN <kulujun@gmail.com>
2024-11-05 17:28:19 +0800

    test | more dataset card style

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,18 +1,37 @@
 import * as React from "react";
-
 import {
   makeStyles,
-  Body1,
-  Caption1,
   Button,
-} from "@fluentui/react-components";
-import { ArrowReplyRegular, ShareRegular } from "@fluentui/react-icons";
-import {
-  Card,
+  Caption1,
+  Text,
+  tokens,
+  Subtitle1,
+  Body1,
+  mergeClasses,
+  PresenceBadge,
   CardFooter,
-  CardHeader,
-  CardPreview,
+  Tooltip,
+  MenuTrigger,
+  MenuPopover,
+  Menu,
+  MenuItem,
+  MenuList,
 } from "@fluentui/react-components";
+import {
+  AlertUrgent16Filled,
+  Attach16Regular,
+  CheckmarkCircle16Regular,
+  CheckmarkCircleFilled,
+  CircleHalfFill16Regular,
+  Comment16Regular,
+  EditFilled,
+  ErrorCircleFilled,
+  HistoryFilled,
+  LightbulbFilamentFilled,
+  MoreHorizontal20Regular,
+  Open16Regular,
+} from "@fluentui/react-icons";
+import { Card, CardHeader, CardPreview } from "@fluentui/react-components";
 
 const resolveAsset = (asset: string) => {
   const ASSET_URL =
@@ -22,49 +41,165 @@ const resolveAsset = (asset: string) => {
 };
 
 const useStyles = makeStyles({
-  card: {
-    margin: "auto",
-    width: "720px",
-    maxWidth: "100%",
+  main: {
+    gap: "36px",
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap",
   },
+
+  card: {
+    width: "360px",
+    maxWidth: "100%",
+    height: "fit-content",
+  },
+
+  section: {
+    width: "fit-content",
+  },
+
+  title: { margin: "0 0 12px" },
+
+  horizontalCardImage: {
+    width: "64px",
+    height: "64px",
+  },
+
+  headerImage: {
+    borderRadius: "4px",
+    maxWidth: "44px",
+    maxHeight: "44px",
+  },
+  flex: {
+    gap: "4px",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  labels: { gap: "6px" },
+
+  footer: { gap: "12px", justifyContent: "space-between" },
+  caption: {
+    color: tokens.colorNeutralForeground3,
+  },
+
+  text: { margin: "0" },
 });
+
+const Title = ({ children }: React.PropsWithChildren<{}>) => {
+  const styles = useStyles();
+
+  return (
+    <Subtitle1 as="h4" block className={styles.title}>
+      {children}
+    </Subtitle1>
+  );
+};
 
 export const About = () => {
   const styles = useStyles();
 
   return (
-    <Card className={styles.card}>
-      <CardHeader
-        image={
-          <img
-            src={resolveAsset("avatar_elvia.svg")}
-            alt="Elvia Atkins avatar picture"
+    <div className={styles.main}>
+      <section className={styles.section}>
+        <Card className={styles.card}>
+          <CardHeader
+            header={
+              <>
+                <Text weight="semibold">Project Test #1</Text>
+              </>
+            }
+            description={
+              <Caption1 className={styles.caption}>这是测试项目</Caption1>
+            }
+            action={
+              <div className={styles.flex}>
+                <Tooltip content="进行中" relationship="label">
+                  <PresenceBadge status="away" />
+                </Tooltip>
+
+                <Body1>45/50</Body1>
+
+                <Menu>
+                  <MenuTrigger disableButtonEnhancement>
+                    <Tooltip content="更多操作" relationship="label">
+                      <Button
+                        appearance="transparent"
+                        icon={<MoreHorizontal20Regular />}
+                        aria-label="More options"
+                      />
+                    </Tooltip>
+                  </MenuTrigger>
+                  <MenuPopover>
+                    <MenuList>
+                      <MenuItem>Item a</MenuItem>
+                      <MenuItem>Item b</MenuItem>
+                    </MenuList>
+                  </MenuPopover>
+                </Menu>
+              </div>
+            }
           />
-        }
-        header={
-          <Body1>
-            <b>Elvia Atkins</b> mentioned you
-          </Body1>
-        }
-        description={<Caption1>5h ago · About us - Overview</Caption1>}
-      />
 
-      <CardPreview
-        logo={
-          <img src={resolveAsset("docx.png")} alt="Microsoft Word document" />
-        }
-      >
-        <img
-          src={resolveAsset("doc_template.png")}
-          alt="Preview of a Word document: About Us - Overview"
-        />
-      </CardPreview>
+          <footer className={mergeClasses(styles.flex, styles.footer)}>
+            <div className={styles.flex}>
+              <CheckmarkCircleFilled primaryFill="#13A10E" fontSize="20px" />
+              <Body1>5</Body1>
+            </div>
+            <div className={styles.flex}>
+              <ErrorCircleFilled primaryFill="#D13438" fontSize="20px" />
+              <Body1>0</Body1>
+            </div>
 
-      <CardFooter>
-        <Button icon={<ArrowReplyRegular fontSize={16} />}>Reply</Button>
-        <Button icon={<ShareRegular fontSize={16} />}>Share</Button>
-      </CardFooter>
-    </Card>
+            <div className={styles.flex}>
+              <LightbulbFilamentFilled primaryFill="#677ED4" fontSize="20px" />
+              <Body1>0</Body1>
+            </div>
+            <div>
+              <div className={styles.flex}>
+                <HistoryFilled primaryFill="#6B6860" />
+                <Text>2024-11-05 14:22:20</Text>
+              </div>
+
+              <div className={styles.flex}>
+                <EditFilled primaryFill="#6B6860" />
+                <Text color="#6B6860">2024-11-05 16:22:20</Text>
+              </div>
+            </div>
+          </footer>
+        </Card>
+      </section>
+
+      <section className={styles.section}>
+        <Title>'horizontal'</Title>
+        <p>With image as part of preview</p>
+
+        <Card className={styles.card} orientation="horizontal">
+          <CardPreview className={styles.horizontalCardImage}>
+            <img
+              className={styles.horizontalCardImage}
+              src={resolveAsset("app_logo.svg")}
+              alt="App Name Document"
+            />
+          </CardPreview>
+
+          <CardHeader
+            header={<Text weight="semibold">App Name</Text>}
+            description={
+              <Caption1 className={styles.caption}>Developer</Caption1>
+            }
+            action={
+              <Button
+                appearance="transparent"
+                icon={<MoreHorizontal20Regular />}
+                aria-label="More options"
+              />
+            }
+          />
+        </Card>
+      </section>
+    </div>
   );
 };
-export default About
+export default About;
